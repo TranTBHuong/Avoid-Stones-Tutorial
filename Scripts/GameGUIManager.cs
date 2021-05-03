9db1d5d7ef54cb48630b1539da222a529323fbc9
TranTBHuong <tranhuong071199@gmail.com>
2021-05-03 19:12:10 +0700

    first commit

---
+++ Scripts/GameGUIManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameGUIManager : Singleton<GameGUIManager>
+{
+    public GameObject homeGui;
+    public GameObject gameGui;
+    public GameObject gameover;
+
+    public Text scoreCoutingText;
+
+    public override void Awake()
+    {
+        MakeSingleton(false);
+    }
+
+    public void ShowGameGui(bool isShow)
+    {
+        if (gameGui)
+        {
+            gameGui.SetActive(isShow);
+        }
+        if (homeGui)
+        {
+            homeGui.SetActive(!isShow);
+        }
+    }
+
+    public void ShowGameover(bool isShow)
+    {
+        if (gameover)
+        {
+            gameover.SetActive(isShow);
+        }
+    }
+
+    public void UpdateScoreCounting(int score)
+    {
+        if (scoreCoutingText)
+        {
+            scoreCoutingText.text = "SCORE : x" + score.ToString("00");
+        }
+    }
+
+    public void Replay()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void Home()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void ExitGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Application.Quit();
+    }
+}
